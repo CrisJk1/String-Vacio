97ddcf486aa3eb2365523911cb71d97155385ead
friquelmel9 <friquelmel@usm.cl>
2023-09-23 14:10:09 -0300

    modified:   Agenda/urls.py 	new file:   main/urls.py 	modified:   main/views.py

---
+++ main/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+#Plantilla para la pagina inicial. Deberia mostrar el index de views.py
+urlpatterns = [
+    path("", views.main, name="main"),
+    path("v1/", views.v1, name="v1"),
+]
