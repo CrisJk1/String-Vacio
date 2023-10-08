386bb679b0f796ae2e83a1112f82fef0c076a57d
Quebinn <kredel@usm.cl>
2023-10-08 17:10:32 -0300

    Base

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import objetivos
 
 # Register your models here.
+
+admin.site.register(objetivos)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class objetivos(models.Model):
+    accion = models.CharField(max_length=30)
+    huella = models.IntegerField()
+    informacion = models.CharField(max_length=400)
