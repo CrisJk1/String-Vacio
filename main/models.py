8a62636ade791296a424d1bc9854b6260152e6e8
friquelmel9 <friquelmel@usm.cl>
2023-10-11 18:20:56 -0300

    modified:   main/models.py

--- main/models.py
+++ main/models.py
@@ -6,3 +6,14 @@ class objetivos(models.Model):
     accion = models.CharField(max_length=30)
     huella = models.IntegerField()
     informacion = models.CharField(max_length=400)
+
+class Usuarios(models.Model):
+    Usuario = models.CharField(max_length=20)
+    Correo = models.CharField(max_length=50)
+    Contraseña = models.CharField(max_length=50)
+
+class RegistroCO(models.Model):
+    Usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+    Fecha = models.DateTimeField(auto_now_add=True)
+    Repositorio = models.CharField()
+
