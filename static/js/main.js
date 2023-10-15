65ef63cbb8d281568d848075dd43a2a2d0738190
CrisJk1 <cquintanilla@usm.cl>
2023-10-15 00:07:06 -0300

    Cris_Jk1

---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var fecha = new Date();
+    var options = { year: 'numeric', month: 'long', day: 'numeric' };
+    var fechaFormateada = fecha.toLocaleDateString("es-ES", options);
+    document.getElementById("fecha").innerText = fechaFormateada;
+  });
