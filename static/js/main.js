69c0102ab9361e081ebe0ef5318d1a300aadd972
CrisJk1 <cquintanilla@usm.cl>
2023-10-28 00:32:04 -0300

    Cris_Jk1|Diosito ayudame a conectar la base de datos

--- static/js/main.js
+++ static/js/main.js
@@ -5,21 +5,53 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("fecha").innerText = fechaFormateada;
   });
 
-var i = 0;
-function myFunction() {
-    var tabla = document.getElementById("miTabla");
-    var fila = tabla.insertRow(-1);
+// var i = 0;
+// function myFunction() {
+//     var tabla = document.getElementById("miTabla");
+//     var fila = tabla.insertRow(-1);
+//
+//     var celda1 = fila.insertCell(0);
+//     var celda2 = fila.insertCell(1);
+//     var celda3 = fila.insertCell(2);
+//
+//     celda1.innerHTML = '<img src="../../static/img/class_pollito.png" alt="Descripción de la imagen">'
+//     celda2.innerHTML = "Comer";
+//     celda3.innerHTML = '<input type="checkbox">';
+//
+//     celda1.className = "uno";
+//     celda2.className = "dos";
+//     celda3.className = "tres";
+//
+//     i++;}
 
-    var celda1 = fila.insertCell(0);
-    var celda2 = fila.insertCell(1);
-    var celda3 = fila.insertCell(2);
 
-    celda1.innerHTML = '<img src="../../static/img/class_pollito.png" alt="Descripción de la imagen">'
-    celda2.innerHTML = "Comer pene";
-    celda3.innerHTML = '<input type="checkbox">';
+function agregarFila() {
+  var tabla = document.getElementById("tabla");
+  var fila = tabla.insertRow(-1);
 
-    celda1.className = "uno";
-    celda2.className = "dos";
-    celda3.className = "tres";
+  var celda1 = fila.insertCell(0);
+  var celda2 = fila.insertCell(1);
+  var celda3 = fila.insertCell(2);
 
-    i++;}
+  var seleccion = document.getElementById("opciones").value;
+
+  switch(seleccion) {
+    case "opcion1":
+      celda1.innerHTML = '<img src="../../static/img/iconoTransporte.png" alt="Descripción de la imagen">';
+      celda2.innerHTML = "xd";
+      celda3.innerHTML = '<input type="checkbox">';
+      break;
+    case "opcion2":
+      celda1.innerHTML = "Opción 2 - Columna 1";
+      celda2.innerHTML = "Opción 2 - Columna 2";
+      celda3.innerHTML = "Opción 2 - Columna 3";
+      break;
+    case "opcion3":
+      celda1.innerHTML = "Opción 3 - Columna 1";
+      celda2.innerHTML = "Opción 3 - Columna 2";
+      celda3.innerHTML = "Opción 3 - Columna 3";
+      break;
+    default:
+      break;
+   }
+}
