bd8d93435198eab14172d6eb9c7c0e03a999e356
CrisJk1 <cquintanilla@usm.cl>
2023-10-29 00:13:47 -0300

    Cris_Jk1|Diosito ayudame a conectar la base de datos

--- static/js/main.js
+++ static/js/main.js
@@ -5,26 +5,6 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("fecha").innerText = fechaFormateada;
   });
 
-// var i = 0;
-// function myFunction() {
-//     var tabla = document.getElementById("miTabla");
-//     var fila = tabla.insertRow(-1);
-//
-//     var celda1 = fila.insertCell(0);
-//     var celda2 = fila.insertCell(1);
-//     var celda3 = fila.insertCell(2);
-//
-//     celda1.innerHTML = '<img src="../../static/img/class_pollito.png" alt="Descripción de la imagen">'
-//     celda2.innerHTML = "Comer";
-//     celda3.innerHTML = '<input type="checkbox">';
-//
-//     celda1.className = "uno";
-//     celda2.className = "dos";
-//     celda3.className = "tres";
-//
-//     i++;}
-
-
 function agregarFila() {
   var tabla = document.getElementById("tabla");
   var fila = tabla.insertRow(-1);
